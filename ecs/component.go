b31f3a10caa9452dca23588d45a9f50d914d6d61
andrei <b1naryth1ef@gmail.com>
2022-12-12 16:47:27 -0800

    cons

--- ecs/component.go
+++ ecs/component.go
@@ -5,9 +5,21 @@ type NameComponent struct {
 	Name string
 }
 
+func NewNameComponent(name string) *NameComponent {
+	return &NameComponent{
+		Name: name,
+	}
+}
+
 /// LabelComponent provides the ability to label entities
 type LabelComponent struct {
 	Labels map[string]string
+}
+
+func NewLabelComponent() *LabelComponent {
+	return &LabelComponent{
+		Labels: make(map[string]string),
+	}
 }
 
 type Component = any
